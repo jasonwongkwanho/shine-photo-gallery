91a6311782eb7d5d6865cb33ee885b53f72b065e
jasonwongkwanho <jasonwongkwanho@gmail.com>
2026-05-19 00:42:11 +0800

    Update gallery config

--- config.js
+++ config.js
@@ -1,1 +1,8 @@
-const GALLERY_CONFIG = {};
+window.GALLERY_CONFIG = {
+  schoolNameZh: "香海正覺蓮社佛教普光學校",
+  schoolNameEn: "HHCLKA Buddhist Po Kwong School",
+  siteTitle: "尚計劃活動相片集",
+  siteSubtitle: "記錄學生參與尚銀行、網尚店、工作體驗及校園服務的學習歷程。",
+  categories: ["全部相簿", "尚銀行", "網尚店", "工作體驗", "校園服務", "其他"],
+  footerText: "HHCLKA Buddhist Po Kwong School"
+};
