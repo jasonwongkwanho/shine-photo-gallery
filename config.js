71752ceaaaaf68b5b71d85cc7c3453f1390809f9
jasonwongkwanho <jasonwongkwanho@gmail.com>
2026-05-19 22:06:01 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ window.GALLERY_CONFIG = {
   schoolNameZh: "香海正覺蓮社佛教普光學校",
   schoolNameEn: "HHCLKA Buddhist Po Kwong School",
   logoUrl: "./assets/school-logo.png",
-  siteTitle: "尚回憶．尚片集",
+  siteTitle: "尚．片集",
   siteSubtitle: "記錄學生的學習歷程，見證每一次參與、嘗試與進步。",
-  categories: ["焦點活動", "大型活動", "工作體驗", "校園服務", "其他"],
+  categories: ["焦點活動", "尚．回憶"],
   footerText: "HHCLKA Buddhist Po Kwong School",
 
   apiBaseUrl: "https://script.google.com/macros/s/AKfycbzOe2BGz98pp1cozsTID82MH21dbpsTokCsPUU04R_LCgf4o8nqaKrEHo45_Ci8LGGONA/exec",
